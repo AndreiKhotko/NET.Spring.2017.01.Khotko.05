0dbf4842d2732c039642a4b9b5837371989f8e0c
AndreiKhotko <khotko.epam@gmail.com>
2017-03-24 04:12:04 +0300

    commit Task1

---
+++ Euclidean.Test/EuclideanGCDTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EuclideanGCD.Test
+{
+    [TestFixture]
+    public class EuclideanGCDTests
+    {
+        [TestCase()]
+        public void GetGCD_TakesZeroParameters_ThrowArgumentCountException()
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD());
+        }
+
+        [TestCase(1)]
+        public void GetGCD_TakesOneParameter_ThrowArgumentCountException(int a)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(a));
+        }
+
+        [TestCase(2, 4, ExpectedResult = 2)]
+        [TestCase(30, 10, ExpectedResult = 10)]
+        public int GetGCD_TakesTwoParameters_PositiveTest(int a, int b)
+        {
+            return EuclideanGCD.GetGCD(a, b);
+        }
+
+        [TestCase(5, 10, 15, ExpectedResult = 5)]
+        [TestCase(30, 2, 15, ExpectedResult = 1)]
+        [TestCase(2, 4, 6, 10, 8, ExpectedResult = 2)]
+        [TestCase(9, 3, 18, 30, 90, ExpectedResult = 3)]
+        public int GetGCD_TakesMoreThanTwoParameters_PositiveTest(params int[] numbers)
+        {
+            return EuclideanGCD.GetGCD(numbers);
+        }
+
+        [TestCase(5, 0)]
+        [TestCase(1, 2, 0)]
+        public void GetGCD_TakesZero_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(numbers));
+        }
+
+        [TestCase(5, -10, 15)]
+        [TestCase(-30, 2, 15)]
+        public void GetGCD_TakesNegativeNumber_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(numbers));
+        }
+    }
+}
---
+++ EuclideanGCD/EuclideanGCD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclideanGCD
+{
+    /// <summary>
+    /// Class that implements Euclidean algorithm
+    /// </summary>
+    public static class EuclideanGCD
+    {
+        /// <summary>
+        /// Method that calculate a greatest common divisor for two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int GetGCD(int a, int b)
+        {
+            if (a <= 0 || b <= 0)
+                throw new ArgumentException();
+
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+
+            return a;
+        }
+
+        /// <summary>
+        /// Method that calculate a greatest common divisor for 2 and more numbers
+        /// </summary>
+        /// <param name="numbers">numbers, count should be two or more</param>
+        public static int GetGCD(params int[] numbers)
+        {
+            if (numbers.Length == 0 || numbers.Length == 1)
+                throw new ArgumentException();
+
+            int tmpGCD = GetGCD(numbers[0], numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                tmpGCD = GetGCD(tmpGCD, numbers[i]);
+            }
+
+            return tmpGCD;
+        }
+    }
+}
