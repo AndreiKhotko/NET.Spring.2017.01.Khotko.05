ce385b030e70c3a86dfd2b69e4a6a748fe258902
AndreiKhotko <khotko.epam@gmail.com>
2017-03-24 05:14:58 +0300

    commit added Binary algorithm and calculation of execution time of methods

--- Euclidean.Test/EuclideanGCDTests.cs
+++ Euclidean.Test/EuclideanGCDTests.cs
@@ -10,23 +10,25 @@ namespace EuclideanGCD.Test
     [TestFixture]
     public class EuclideanGCDTests
     {
+        private long timeExecution;
+
         [TestCase()]
         public void GetGCD_TakesZeroParameters_ThrowArgumentCountException()
         {
-            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD());
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(out timeExecution));
         }
 
         [TestCase(1)]
         public void GetGCD_TakesOneParameter_ThrowArgumentCountException(int a)
         {
-            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(a));
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(out timeExecution, a));
         }
 
         [TestCase(2, 4, ExpectedResult = 2)]
         [TestCase(30, 10, ExpectedResult = 10)]
         public int GetGCD_TakesTwoParameters_PositiveTest(int a, int b)
         {
-            return EuclideanGCD.GetGCD(a, b);
+            return EuclideanGCD.GetGCD(out timeExecution, a, b);
         }
 
         [TestCase(5, 10, 15, ExpectedResult = 5)]
@@ -35,21 +37,77 @@ namespace EuclideanGCD.Test
         [TestCase(9, 3, 18, 30, 90, ExpectedResult = 3)]
         public int GetGCD_TakesMoreThanTwoParameters_PositiveTest(params int[] numbers)
         {
-            return EuclideanGCD.GetGCD(numbers);
+            return EuclideanGCD.GetGCD(out timeExecution, numbers);
         }
 
-        [TestCase(5, 0)]
-        [TestCase(1, 2, 0)]
-        public void GetGCD_TakesZero_ThrowsArgumentException(params int[] numbers)
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(1, 2, 0, ExpectedResult = 1)]
+        public int GetGCD_TakesZero_PositiveTest(params int[] numbers)
         {
-            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(numbers));
+            return EuclideanGCD.GetGCD(out timeExecution, numbers);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 0, 0)]
+        public void GetGCD_TakesAllZero_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(out timeExecution, numbers));
         }
 
         [TestCase(5, -10, 15)]
         [TestCase(-30, 2, 15)]
         public void GetGCD_TakesNegativeNumber_ThrowsArgumentException(params int[] numbers)
         {
-            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(numbers));
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.GetGCD(out timeExecution, numbers));
+        }
+
+        [TestCase()]
+        public void BinaryGCD_TakesZeroParameters_ThrowArgumentCountException()
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.BinaryGCD(out timeExecution));
+        }
+
+        [TestCase(1)]
+        public void BinaryGCD_TakesOneParameter_ThrowArgumentCountException(int a)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.BinaryGCD(out timeExecution, a));
+        }
+
+        [TestCase(2, 4, ExpectedResult = 2)]
+        [TestCase(30, 10, ExpectedResult = 10)]
+        public int BinaryGCD_TakesTwoParameters_PositiveTest(int a, int b)
+        {
+            return EuclideanGCD.BinaryGCD(out timeExecution, a, b);
+        }
+
+        [TestCase(5, 10, 15, ExpectedResult = 5)]
+        [TestCase(30, 2, 15, ExpectedResult = 1)]
+        [TestCase(2, 4, 6, 10, 8, ExpectedResult = 2)]
+        [TestCase(9, 3, 18, 30, 90, ExpectedResult = 3)]
+        public int BinaryGCD_TakesMoreThanTwoParameters_PositiveTest(params int[] numbers)
+        {
+            return EuclideanGCD.BinaryGCD(out timeExecution, numbers);
+        }
+
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(1, 2, 0, ExpectedResult = 1)]
+        public int BinaryGCD_TakesZero_PositiveTest(params int[] numbers)
+        {
+            return EuclideanGCD.BinaryGCD(out timeExecution, numbers);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 0, 0)]
+        public void BinaryGCD_TakesAllZero_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.BinaryGCD(out timeExecution, numbers));
+        }
+
+        [TestCase(5, -10, 15)]
+        [TestCase(-30, 2, 15)]
+        public void BinaryGCD_TakesNegativeNumber_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(() => EuclideanGCD.BinaryGCD(out timeExecution, numbers));
         }
     }
 }
--- EuclideanGCD/EuclideanGCD.cs
+++ EuclideanGCD/EuclideanGCD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,30 @@ namespace EuclideanGCD
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
+        /// <param name="timeExecutionInMilliSeconds"></param>
         /// <returns></returns>
-        public static int GetGCD(int a, int b)
+        private static int GetGCD(out long timeExecutionInMilliSeconds, int a, int b)
         {
-            if (a <= 0 || b <= 0)
+            var watch = Stopwatch.StartNew();
+
+            if (a < 0 || b < 0)
+                throw new ArgumentException();
+
+            if (a == 0 && b != 0)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return b;
+            }
+
+            if (b == 0 && a != 0)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return a;
+            }
+
+            if (a == 0 && b == 0)
                 throw new ArgumentException();
 
             while (a != b)
@@ -30,6 +51,10 @@ namespace EuclideanGCD
                     b -= a;
             }
 
+            watch.Stop();
+
+            timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+
             return a;
         }
 
@@ -37,17 +62,121 @@ namespace EuclideanGCD
         /// Method that calculate a greatest common divisor for 2 and more numbers
         /// </summary>
         /// <param name="numbers">numbers, count should be two or more</param>
-        public static int GetGCD(params int[] numbers)
+        /// <param name="timeExecutionInMilliSeconds"></param>
+        public static int GetGCD(out long timeExecutionInMilliSeconds, params int[] numbers)
         {
+            var watch = Stopwatch.StartNew();
+
             if (numbers.Length == 0 || numbers.Length == 1)
                 throw new ArgumentException();
 
-            int tmpGCD = GetGCD(numbers[0], numbers[1]);
+            if (numbers.All(number => number == 0))
+                throw new ArgumentException();
+
+            long tmpTime;
+            int tmpGCD = GetGCD(out tmpTime, numbers[0], numbers[1]);
 
             for (int i = 2; i < numbers.Length; i++)
             {
-                tmpGCD = GetGCD(tmpGCD, numbers[i]);
+                tmpGCD = GetGCD(out tmpTime, tmpGCD, numbers[i]);
             }
+
+            watch.Stop();
+
+            timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+
+            return tmpGCD;
+        }
+
+        /// <summary>
+        /// Method that calculate a greatest common divisor for 2 and more numbers using binary algorithm
+        /// </summary>
+        /// <param name="numbers">numbers, count should be two or more</param>
+        /// <param name="timeExecutionInMilliSeconds"></param>
+        private static int BinaryGCD(out long timeExecutionInMilliSeconds, int a, int b)
+        {
+            var watch = Stopwatch.StartNew();
+            long tmp;
+
+            if (a < 0 || b < 0)
+                throw new ArgumentException();
+            if (a == 0)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return b;
+            }
+            if (b == 0)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return a;
+            }
+            if (a == b)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return a;
+            }
+            if (a == 1 || b == 1)
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                return 1;
+            }
+            if ((a % 2 == 0) && (b % 2 == 0))
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+
+                return 2 * BinaryGCD(out tmp, a / 2, b / 2);
+            }
+            if ((a % 2 == 0) && (b % 2 != 0))
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                
+                return BinaryGCD(out tmp, a / 2, b);
+            }
+            if ((a % 2 != 0) && (b % 2 == 0))
+            {
+                watch.Stop();
+                timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+                
+                return BinaryGCD(out tmp, a, b / 2);
+            }
+
+            watch.Stop();
+            timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
+
+            return BinaryGCD(out tmp, b, Math.Abs(a - b));
+        }
+
+        /// <summary>
+        /// Method that calculate a greatest common divisor for 2 and more numbers using binary algorithm
+        /// </summary>
+        /// <param name="numbers">numbers, count should be two or more</param>
+        /// <param name="timeExecutionInMilliSeconds"></param>
+        public static int BinaryGCD(out long timeExecutionInMilliSeconds, params int[] numbers)
+        {
+            var watch = Stopwatch.StartNew();
+
+            if (numbers.Length == 0 || numbers.Length == 1)
+                throw new ArgumentException();
+
+            if (numbers.All(number => number == 0))
+                throw new ArgumentException();
+
+            long tmpTime;
+            int tmpGCD = BinaryGCD(out tmpTime, numbers[0], numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                tmpGCD = BinaryGCD(out tmpTime, tmpGCD, numbers[i]);
+            }
+
+            watch.Stop();
+            timeExecutionInMilliSeconds = watch.ElapsedMilliseconds;
 
             return tmpGCD;
         }
