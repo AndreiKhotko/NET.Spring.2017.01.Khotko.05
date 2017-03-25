13414ec2f8c016e83ba019daf7bec39bd5518fb0
AndreiKhotko <khotko.epam@gmail.com>
2017-03-25 17:22:03 +0300

    add DoubleExtention Task

---
+++ DoubleExtention/DoubleExtention.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleExtention
+{
+    /// <summary>
+    /// Class that implement an extention method for double
+    /// </summary>
+    public static class DoubleExtention
+    {
+        /// <summary>
+        /// Method that returns a string of binary representaion of double value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Binary String</returns>
+        public static string ToBinarySting(this double value)
+        {
+            long int64Bits = BitConverter.DoubleToInt64Bits(value);
+
+            string result = string.Empty;
+            
+            for (int i = 63; i >= 0; i--)
+            {
+                result += Convert.ToString((int64Bits >> i) & 1);
+            }
+            
+            return result;
+        }
+    }
+}
